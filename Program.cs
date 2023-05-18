10d5db33cdad5d24a427e345ca878350b901485c
Yahir <yahir.chi.chi@gmail.com>
2023-05-18 12:03:20 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Poo_Estructura.Servicios;
+using System;
+
+namespace Poo_Estructura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlumnoServicio alumnoServicio = new AlumnoServicio();
+            var res = alumnoServicio.Agregar();
+            alumnoServicio.Imprimir(res);
+        }
+    }
+}
